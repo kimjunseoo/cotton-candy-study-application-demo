a0dc198b3352c297c4007f9fc12d8df5f03b1ef8
kimjunseoo <zz0905k@naver.com>
2023-02-08 19:19:00 +0900

    after_setting

---
+++ server.js
@@ -0,0 +1,10 @@
+import express from "express";
+import http from "http";
+import { Server } from "socket.io";
+
+const app = express();
+
+const server = http.createServer(app);
+const wsServer = new Server(server);
+
+server.listen(3002, () => { console.log("*****HTTP/WS SERVER ON*****")});
